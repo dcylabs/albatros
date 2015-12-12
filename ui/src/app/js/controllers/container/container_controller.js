49f4c3242030dbbb304facd5fc103a58b85220d7
dcylabs <dcylabs@gmail.com>
2015-12-12 15:36:57 +0100

    Improving live console

--- ui/src/app/js/controllers/container/container_controller.js
+++ ui/src/app/js/controllers/container/container_controller.js
@@ -69,7 +69,7 @@ angular.module("app").controller('ContainerController', function($scope, $routeP
 
   $scope.refreshLogs = function(){
     DockerResource.Containers.logs({id:$routeParams.id},function(data){
-      $scope.logs = data;
+      $scope.logs = resourceDataToStr(data);
     });
   };
 
@@ -111,11 +111,7 @@ angular.module("app").controller('ContainerController', function($scope, $routeP
   }; 
 
   $scope.send = function(){
-    var command = $scope.command+"\n"; 
     $scope.command = ""; 
-    if(dataStream){
-      dataStream.send(command);   
-    }
   };
  
   $scope.initConsole = function(){
@@ -125,18 +121,50 @@ angular.module("app").controller('ContainerController', function($scope, $routeP
     dataStream = DockerResource.getConsoleContainer({id:$routeParams.id});
     if(dataStream){
       dataStream.onMessage(function(message){
-        $scope.console += message.data;
+        console.log(message.data);
+        message.data.split('').every(function(character){
+          var charCode = character.charCodeAt(0); 
+          if(charCode !== 0){
+            if(charCode == 8){
+              //backspace
+              $scope.console = $scope.console.replace(/.(\x1B\[[^a-z]*[a-z])?$/gi,'');
+
+              // \x1B\[([^a-z]+)[a-z]/gi
+            }else{
+              //add
+              $scope.console += character;
+            }
+          }
+          return charCode !== 0; 
+        });
         $('.console .output .content').stop().animate({scrollTop: $('.console .output .content')[0].scrollHeight},100);
       });
     }     
   };
 
+  $scope.handleConsoleKeyboard = function($event){
+    var character;    
+    if($event.ctrlKey && $event.keyCode >= 65 && $event.keyCode <= 95){
+      character = String.fromCharCode($event.keyCode - 64);
+    }else if($event.key.length == 1){
+      character = $event.key;
+    }else if($event.keyCode >= 37 && $event.keyCode <= 40){
+      if($event.keyCode == 38){
+        character = String.fromCharCode($event.keyCode - 22);
+      }
+    }else if($event.key == 'Shift'){
+      // unhandled arrow keys in html
+    }else{
+      character = String.fromCharCode($event.keyCode);
+    }
+    if(dataStream){
+      dataStream.send(character);   
+    }
+  };
+
 	$scope.refreshContainer = function(){
   	DockerResource.Containers.get({id:$routeParams.id}, function(data){
       $scope.container = data; 
-      if($scope.container.isUp){
-
-      }
     });
     $scope.refreshTop(); 
     $scope.refreshLogs(); 
